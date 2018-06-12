6c8710b3a1556c40fdf879e5020cb60c3ea47976
Wuyuan <wuyuan@pureengineering.com>
2018-06-12 15:39:12 -0700

    added poly to a function

--- ball.m
+++ ball.m
@@ -38,18 +38,10 @@ for index=1:step:length(S)
     Strim = S(index:index+step, 1);
     angleTrim = angle(index:index+step, 1);
     
-    xcT = Strim.*cos(angleTrim);
-    ycT = Strim.*sin(angleTrim);
-    lastCol = ones(length(ycT),1);
+    %calculate the least squares
+    [xcT,xcT_est] = myFunc(Strim, angleTrim);
     
-    A = [lastCol ycT ycT.^2];
-    xhat = A\xcT;
-    xcT_est = A*xhat;
-    pause(0.5);
-    plot(ycT, xcT_est, '--o');
-    leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',xcT_est(1),xcT_est(2),xcT_est(3))
-    legend('Data',leg_est)
-    
+    %find the average sum of residual
     res =(xcT - xcT_est).^2;
     for k=1:length(res)
         resSum = resSum + res(k);
@@ -62,8 +54,7 @@ for index=1:step:length(S)
     
 end
 
-
-
+%match each residual to a counter
 keySet = aveSumRec;
 valueSet = countRec;
 key = min(aveSumRec);
@@ -80,17 +71,7 @@ plot(yc, xc); hold on
 
 Strim = S(loc_start:loc_end, 1);
 angleTrim = angle(loc_start:loc_end, 1);
-
-xcT = Strim.*cos(angleTrim);
-ycT = Strim.*sin(angleTrim);
-lastCol = ones(length(ycT),1);
-
-A = [lastCol ycT ycT.^2];
-xhat = A\xcT;
-xcT_est = A*xhat;
-plot(ycT, xcT_est, '--o');
-leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',xcT_est(1),xcT_est(2),xcT_est(3))
-legend('Data',leg_est)
+[xcT,xcT_est] = myFunc(Strim, angleTrim)
 
 
 %--------------------works-----------------------------------
@@ -102,4 +83,17 @@ legend('Data',leg_est)
 % leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',beta(1),beta(2),beta(3))
 % legend('Data',leg_est)
 
-
+function [xcT,xcT_est] = myFunc(Strim, angleTrim)
+    
+    xcT = Strim.*cos(angleTrim);
+    ycT = Strim.*sin(angleTrim);
+    lastCol = ones(length(ycT),1);
+    
+    A = [lastCol ycT ycT.^2];
+    xhat = A\xcT;
+    xcT_est = A*xhat;
+    pause(0.5);
+    plot(ycT, xcT_est, '--o');
+    leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',xcT_est(1),xcT_est(2),xcT_est(3))
+    legend('Data',leg_est)
+end
