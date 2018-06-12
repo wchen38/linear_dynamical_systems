cea342f15f0b037466ca7d79d3f392021ae38b4d
Wuyuan <wuyuan@pureengineering.com>
2018-06-12 15:18:19 -0700

    can detect a circle

--- ball.m
+++ ball.m
@@ -12,58 +12,85 @@ angle =[ -1.57079637051:0.00436332309619:1.56643295288];
 %ball data points start at 305 end at 440
 start = 305;
 N = 440;
-
+res = 0;
+xc_est = zeros(3,1);
 resSum = 0;
-
+count = 0;
+aveSumRec = [];
+countRec = [];
 xc = S.*cos(angle);
 yc = S.*sin(angle);
 
-plot(angle, S, '*');
-xlabel('angle'), ylabel('S'); 
+plot(yc, xc); hold on
+xlabel('x'), ylabel('y');
 title('Cylindrial Measurement Data before Trimming')
-figure 
 
-S = S(1,start:N);
 S = S';
-angle = angle(1, start:N);
 angle = angle';
 
+step = 110;%abs(start-N);
 
-xc = S.*cos(angle);
-yc = S.*sin(angle);
-lastCol = ones(length(yc),1);
-
-
-plot(angle, S, '*');
-xlabel('angle'), ylabel('distance');
-title('Cylindrial Measurement Data After trimming')
-figure
-
-plot(yc, xc, '*');  hold on
-A = [lastCol yc yc.^2];
-xhat = A\xc;
-xc_est = A*xhat;
-plot(yc, xc_est);
-leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',xc_est(1),xc_est(2),xc_est(3))
-legend('Data',leg_est)
-
-
-res =(xc - xc_est).^2;
-for k=1:length(res)
-    resSum = resSum + res(k);
+for index=1:step:length(S)
+    resSum = 0;
+    if(index+step >= length(S))
+        break;
+    end 
+    Strim = S(index:index+step, 1);
+    angleTrim = angle(index:index+step, 1);
+    
+    xcT = Strim.*cos(angleTrim);
+    ycT = Strim.*sin(angleTrim);
+    lastCol = ones(length(ycT),1);
+    
+    A = [lastCol ycT ycT.^2];
+    xhat = A\xcT;
+    xcT_est = A*xhat;
+    pause(0.5);
+    plot(ycT, xcT_est, '--o');
+    leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',xcT_est(1),xcT_est(2),xcT_est(3))
+    legend('Data',leg_est)
+    
+    res =(xcT - xcT_est).^2;
+    for k=1:length(res)
+        resSum = resSum + res(k);
+    end
+    aveSum = resSum/length(res);
+    aveSumRec = [aveSumRec aveSum];
+    count = count + 1;
+    countRec = [countRec count];
+    
+    
 end
 
-aveSum = resSum/length(res);
 
-keySet = {0.000900001 0.000900002};
-valueSet = {1 2};
 
-key = 0.000900001;
-
+keySet = aveSumRec;
+valueSet = countRec;
+key = min(aveSumRec);
 M = containers.Map(keySet, valueSet);
 
-M(key)
 
+pause(1);
+%---------------find the ball -------------------------%
+loc_start = step*M(key) - step;
+loc_end = step*M(key);
+
+figure 
+plot(yc, xc); hold on
+
+Strim = S(loc_start:loc_end, 1);
+angleTrim = angle(loc_start:loc_end, 1);
+
+xcT = Strim.*cos(angleTrim);
+ycT = Strim.*sin(angleTrim);
+lastCol = ones(length(ycT),1);
+
+A = [lastCol ycT ycT.^2];
+xhat = A\xcT;
+xcT_est = A*xhat;
+plot(ycT, xcT_est, '--o');
+leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',xcT_est(1),xcT_est(2),xcT_est(3))
+legend('Data',leg_est)
 
 
 %--------------------works-----------------------------------
