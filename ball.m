62fd0184f4897e8726b22572d7862d1937cc8add
Wuyuan <wuyuan@pureengineering.com>
2018-06-12 18:52:47 -0700

    able to find circle by swiping through values

--- ball.m
+++ ball.m
@@ -6,12 +6,13 @@ filename1 = './ballStillData/lidar_ball_still.csv'
 position = csvread(filename1);
 
 
+
 S = position(1,:);         %all the yc axis data from csv file
 angle =[ -1.57079637051:0.00436332309619:1.56643295288];
 
 %ball data points start at 305 end at 440
-start = 305;
-N = 440;
+start = 303;
+N = 442;
 res = 0;
 xc_est = zeros(3,1);
 resSum = 0;
@@ -28,9 +29,9 @@ title('Cylindrial Measurement Data before Trimming')
 S = S';
 angle = angle';
 
-step = 110;%abs(start-N);
+step = 137;%abs(start-N);
 
-for index=1:step:length(S)
+for index=1:1:length(S)
     resSum = 0;
     if(index+step >= length(S))
         break;
@@ -48,8 +49,10 @@ for index=1:step:length(S)
     end
     aveSum = resSum/length(res);
     aveSumRec = [aveSumRec aveSum];
+    
     count = count + 1;
     countRec = [countRec count];
+    
     
     
 end
@@ -58,16 +61,18 @@ end
 keySet = aveSumRec;
 valueSet = countRec;
 key = min(aveSumRec);
-M = containers.Map(keySet, valueSet);
+%M = containers.Map(keySet, valueSet);
 
 
 pause(1);
 %---------------find the ball -------------------------%
-loc_start = step*M(key) - step;
-loc_end = step*M(key);
+%loc_start = step*M(key) - step;
+%loc_end = step*M(key);
+loc_start = find(aveSumRec==key);
+loc_end = loc_start + step;
 
 figure 
-plot(yc, xc); hold on
+plot(yc, xc, '*'); hold on
 
 Strim = S(loc_start:loc_end, 1);
 angleTrim = angle(loc_start:loc_end, 1);
@@ -92,7 +97,7 @@ function [xcT,xcT_est] = myFunc(Strim, angleTrim)
     A = [lastCol ycT ycT.^2];
     xhat = A\xcT;
     xcT_est = A*xhat;
-    pause(0.5);
+    %pause(0.5);
     plot(ycT, xcT_est, '--o');
     leg_est=sprintf('Estimated (y=%.4f+%.4fx+%.4fx^2',xcT_est(1),xcT_est(2),xcT_est(3))
     legend('Data',leg_est)
